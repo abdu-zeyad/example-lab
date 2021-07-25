f34f0e32725404bce7db17048a3740eda74ebec8
abdu-zeyad <abd.d@live.com>
2021-07-25 15:50:23 +0300

    sad

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,36 @@
+print(' ************************************** \n ** Welcome to the Snakes Cafe! **\n ** Please see our menu below. **\n **\n ** To quit at any time, type "quit" **\n **************************************'
+      )
+
+things = [
+    {
+        "name": "Appetizers",
+        "hints": [
+            "Wings",
+            "Cookies",
+            "Spring Rolls",
+        ],
+    },
+    {
+        "name": "Entrees",
+        "hints": [
+            "Salmon",
+            "Meat Tornado",
+            "A Literal Garden",
+        ],
+    },
+    {
+        "name": "Desserts",
+        "hints": [
+            "Ice Cream",
+            "Cake",
+            "Pie",
+        ],
+    }, {
+        "name": "Drinks",
+        "hints": [
+            "Coffee",
+            "Tea",
+            "Unicorn Tears",
+        ],
+    },
+]
