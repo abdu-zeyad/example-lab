734addceff08b98e168c5c9155bef6de247f16fc
abdu-zeyad <abd.d@live.com>
2021-07-25 17:28:35 +0300

    As

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,36 +1,54 @@
-print(' ************************************** \n ** Welcome to the Snakes Cafe! **\n ** Please see our menu below. **\n **\n ** To quit at any time, type "quit" **\n **************************************'
-      )
 
-things = [
-    {
-        "name": "Appetizers",
-        "hints": [
-            "Wings",
-            "Cookies",
-            "Spring Rolls",
-        ],
-    },
-    {
-        "name": "Entrees",
-        "hints": [
-            "Salmon",
-            "Meat Tornado",
-            "A Literal Garden",
-        ],
-    },
-    {
-        "name": "Desserts",
-        "hints": [
-            "Ice Cream",
-            "Cake",
-            "Pie",
-        ],
-    }, {
-        "name": "Drinks",
-        "hints": [
-            "Coffee",
-            "Tea",
-            "Unicorn Tears",
-        ],
-    },
-]
+
+menu = """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+"""
+print(menu)
+list_food = []
+
+for i in range(7):
+
+    order = input()
+    if order in list_food:
+        print(f"{len(list_food)} order of {order} have been added to your meal")
+        list_food['order'].append(order)
+        print(len(list_food[order]))
+    else:
+        list_food.append(order)
+        print(f"1 order of {order} have been added to your meal")
+        print(list_food)
+print(list_food)
